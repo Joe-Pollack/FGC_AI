e602ece6e506967c2bdd22b4a097463efd449865
Joe-Pollack <126018568+Joe-Pollack@users.noreply.github.com>
2023-02-21 11:52:04 -0800

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,80 @@
+import tensorflow as tf
+import matplotlib
+from matplotlib import pyplot as plt
+
+matplotlib.rcParams['figure.figsize'] = [9, 6]
+     
+
+x = tf.linspace(-2, 2, 201)
+x = tf.cast(x, tf.float32)
+
+def f(x):
+  y = x**2 + 2*x - 5
+  return y
+
+y = f(x) + tf.random.normal(shape=[201])
+
+plt.plot(x.numpy(), y.numpy(), '.', label='Data')
+plt.plot(x, f(x), label='Ground truth')
+plt.legend();
+
+class Model(tf.Module):
+    def __init__(self):
+        # Randomly generate weight and bias terms
+        rand_init = tf.random.uniform(shape=[3], minval=0., maxval=5., seed=22)
+        # Initialize model parameters
+        self.w_q = tf.Variable(rand_init[0])
+        self.w_l = tf.Variable(rand_init[1])
+        self.b = tf.Variable(rand_init[2])
+        
+    @tf.function
+    def __call__(self,x):
+        return self.w_q * (x**2) + self.w_l * x + self.b
+    
+quad_model = Model()
+
+def plot_preds(x, y, f, model, title):
+    plt.figure()
+    plt.plot(x, y, '.', label='Data')
+    plt.plot(x, f(x), label='Ground truth')
+    plt.plot(x, model(x), label='Predictions')
+    plt.title(title)
+    plt.legend()
+    
+plot_preds(x, y, f, quad_model, 'Before training')
+
+# mean square error loss fn
+def mse_loss(y_pred, y):
+  return tf.reduce_mean(tf.square(y_pred - y))
+
+# get a batch of 32 random points from the dataset
+batch_size = 32
+dataset = tf.data.Dataset.from_tensor_slices((x, y))
+dataset = dataset.shuffle(buffer_size=x.shape[0]).batch(batch_size)
+
+# Set training parameters
+epochs = 100
+learning_rate = 0.01
+losses = []
+
+# Format training loop
+for epoch in range(epochs):
+  for x_batch, y_batch in dataset:
+    with tf.GradientTape() as tape:
+      batch_loss = mse_loss(quad_model(x_batch), y_batch)
+    # Update parameters with respect to the gradient calculations
+    grads = tape.gradient(batch_loss, quad_model.variables)
+    for g,v in zip(grads, quad_model.variables):
+        v.assign_sub(learning_rate*g)
+  # Keep track of model loss per epoch
+  loss = mse_loss(quad_model(x), y)
+  losses.append(loss)
+  if epoch % 10 == 0:
+    print(f'Mean squared error for step {epoch}: {loss.numpy():0.3f}')
+
+# Plot model results
+print("\n")
+plt.plot(range(epochs), losses)
+plt.xlabel("Epoch")
+plt.ylabel("Mean Squared Error (MSE)")
+plt.title('MSE loss vs training iterations');
